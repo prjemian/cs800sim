a9a746981523a8337f506cccf002da2e847e7d65
Pete R Jemian <prjemian@gmail.com>
2020-04-01 15:45:40 -0500

    another example

---
+++ example2/client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+client
+
+see: https://docs.python.org/3.5/library/socketserver.html#socketserver-udpserver-example
+"""
+
+import socket
+import sys
+
+HOST, PORT = "localhost", 9999
+data = " ".join(sys.argv[1:])
+
+# SOCK_DGRAM is the socket type to use for UDP sockets
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# As you can see, there is no connect() call; UDP has no connections.
+# Instead, data is directly sent to the recipient via sendto().
+sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
+received = str(sock.recv(1024), "utf-8")
+
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
