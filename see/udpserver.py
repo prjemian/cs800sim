b2d0bb56d514d318559c9733667fafb64747d2eb
Pete R Jemian <prjemian@gmail.com>
2020-04-01 15:38:42 -0500

    MNT

--- see/udpserver.py
+++ see/udpserver.py
@@ -9,10 +9,10 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
 udp_host = socket.gethostname()		        # Host IP
 udp_port = 12345			                # specified port to connect
 
-#print type(sock) ============> 'type' can be used to see type 
-				# of any variable ('sock' here)
-
 sock.bind((udp_host,udp_port))
+
+print("UDP server IP:", udp_host)
+print("UDP server Port:", udp_port)
 
 while True:
 	print("Waiting for client...")
