d3dd02e77dac5cf63da36bba95723503814c6b3f
Pete R Jemian <prjemian@gmail.com>
2020-04-02 11:53:20 -0500

    refactor

--- v1/utils.py
+++ v1/utils.py
@@ -4,46 +4,47 @@
 utilities
 """
 
-
+import collections
 import psutil
 import socket
 
 
-def isNicKnownAsInternal(nic_name):
-    """
-    is this network interface name known to be internal?
-    """
-    if nic_name == "lo":
-        return True
-    elif nic_name.startswith("Loopback"):
-        return True
-    elif nic_name.startswith("br-"):
-        return True
-    elif nic_name.startswith("Virtual"):
-        return True
-    elif nic_name.startswith("Bluetooth"):
-        return True
-    elif nic_name.startswith("docker"):
-        return True
-    return False
+# def isNicKnownAsInternal(nic_name):
+#     """
+#     is this network interface name known to be internal?
+#     """
+#     if nic_name == "lo":
+#         return True
+#     elif nic_name.startswith("Loopback"):
+#         return True
+#     elif nic_name.startswith("br-"):
+#         return True
+#     elif nic_name.startswith("Virtual"):
+#         return True
+#     elif nic_name.startswith("Bluetooth"):
+#         return True
+#     elif nic_name.startswith("docker"):
+#         return True
+#     return False
 
 
-def isAddrKnownInternal(nic_addr):
-    """
-    is this network interface known to be internal?
-    """
-    if nic_addr.family not in (2, 17):  # (AF_INET, AF_LINK)
-        return True
-    return False
+# def isAddrKnownInternal(nic_addr):
+#     """
+#     is this network interface known to be internal?
+#     """
+#     if nic_addr.family not in (2, 17):  # (AF_INET, AF_LINK)
+#         return True
+#     return False
 
 
 def getActiveIPconnections():
     """
-    get the list of IP numbers that have extablished connections
+    get the list of IP numbers that have established connections
 
-    exclude any local network interfaces
+    * exclude any local network interfaces
+    * return list sorted by greatest number of established connections
     """
-    ip_dict = {}
+    ip_dict = collections.defaultdict(int)
     for conn in psutil.net_connections():
         ip = conn.laddr.ip
         if (
@@ -51,13 +52,16 @@ def getActiveIPconnections():
                 and ip.split(".")[0] not in "0 127 169".split()
                 and conn.status == "ESTABLISHED"
             ):
-            ip_dict[ip] = True
-    return sorted(list(ip_dict.keys()))
+            ip_dict[ip] += 1
+    def sorter(ip):
+        return ip_dict[ip]
+    return sorted(list(ip_dict.keys()), key=sorter, reverse=True)
 
 
-def get_mac():
-    # https://stackoverflow.com/a/41076835/1046449
-
+def getActiveNetworkInterfaces():
+    """
+    return list of the active network interfaces, excluding local or loopback
+    """
     active_ip_connections = getActiveIPconnections()
 
     interfaces = psutil.net_if_addrs()
@@ -66,19 +70,33 @@ def get_mac():
         for conn in nic:
             if conn.address in active_ip_connections:
                 active_interfaces[k] = True
-    active_interfaces = list(active_interfaces.keys())
+    return list(active_interfaces.keys())
 
-    macs = []
-    inet_connections = [
+
+def get_mac():
+    """
+    return a list of the active MAC addresses, sorted by most connections first
+
+    see also: https://stackoverflow.com/a/41076835/1046449
+    """
+    excluded_inet_connection_families = [
         socket.AddressFamily.AF_INET,
         socket.AddressFamily.AF_INET6,
-    ]
-    for nic in active_interfaces:
+        ]
+    hexadecimal_characters = "0123456789abcdef"
+
+    interfaces = psutil.net_if_addrs()
+    macs = []
+    for nic in getActiveNetworkInterfaces():
         for conn in interfaces[nic]:
             # find the MAC address by excluding the other connections
-            if conn.family not in inet_connections:
-                mac = conn.address.replace("-", "").replace(":", "")
-                macs.append(mac)
+            if conn.family not in excluded_inet_connection_families:
+                mac = "".join([
+                    c
+                    for c in conn.address
+                    if c.lower() in hexadecimal_characters])
+                if len(mac) == 12:
+                    macs.append(mac)
 
     return macs
 
@@ -109,4 +127,4 @@ def bs2i(bs):
 
 
 if __name__ == "__main__":
-    get_mac()
+    print(get_mac())
